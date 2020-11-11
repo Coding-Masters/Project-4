38be44578b14f04dc931313567cfb461d76ab9dc
AbdLaswi <abed.laswii@gmail.com>
2020-11-11 20:47:00 +0200

    add Comment, users, Role on dataBase

--- back-end/schemas/schemas.sql
+++ back-end/schemas/schemas.sql
@@ -1,21 +1,40 @@
 -- write the database name that you use here
 USE db_name;
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT NOT NULL,
-   
-    PRIMARY KEY (id)
+    role_id INT NOT NULL,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    address VARCHAR(255),
+    phone VARCHAR(255),
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP,
+    is_deleted TINYINT DEFAULT 0,
+    PRIMARY KEY (id, role_id)
 );
-
 CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL,
-   
+    type VARCHAR(255),
     PRIMARY KEY (id)
 );
-
--- example:
-CREATE TABLE products (
-    id INT AUTO_INCREMENT NOT NULL,
-   
-    PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS mydb.Comments (
+    id INT NOT NULL,
+    posts_id INT NOT NULL,
+    posts_users_id INT NOT NULL,
+    users_id INT NOT NULL,
+    users_role_id INT NOT NULL,
+    posts_subcategories_id INT NOT NULL,
+    posts_subcategories_categories_idcategories INT NOT NULL,
+    masege VARCHAR(45) NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP,
+    PRIMARY KEY (
+        id,
+        posts_id,
+        posts_users_id,
+        users_id,
+        users_role_id,
+        posts_subcategories_id,
+        posts_subcategories_categories_idcategories
+    ),
