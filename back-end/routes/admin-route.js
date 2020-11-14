5a3bebd96d9ee4cb2cded3e75c91269f6523081a
aihamjarrah <ayham.mjarr@gmail.com>
2020-11-14 18:49:32 +0200

    Created admin route and APIs

--- back-end/routes/admin-route.js
+++ back-end/routes/admin-route.js
@@ -57,11 +57,19 @@ adminRouter.get("/posts", (req, res) => {
 });
 
 //APIs For creating, updating, and deleting
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //An endpoint for creating new user
 adminRouter.post("/create/user", (req, res) => {
   const query = `INSERT INTO user (username,email,password,address,Phone,Role_idRole) VALUES (?,?,?,?,?,?)`;
-  const data = [req.body.userName,req.body.email,req.body.password,req.body.address,req.body.phone,req.body.idRole]
-  db.query(query,data, (err, result, fields) => {
+  const data = [
+    req.body.userName,
+    req.body.email,
+    req.body.password,
+    req.body.address,
+    req.body.phone,
+    req.body.idRole,
+  ];
+  db.query(query, data, (err, result, fields) => {
     if (err) throw err;
     console.log(result);
     res.json(`User ${req.body.userName} added successfuly`);
@@ -69,32 +77,36 @@ adminRouter.post("/create/user", (req, res) => {
 });
 
 //An endpoint for deleting a user from the database
-adminRouter.delete("delete/user", (req, res) => {
+adminRouter.delete("/delete/user", (req, res) => {
   const query = `DELETE FROM user WHERE username = ?`;
-  const data = [req.body.userName]
-  db.query(query,data, (err, result, field) => {
-    if(err) throw err;
+  const data = [req.body.userName];
+  db.query(query, data, (err, result, field) => {
+    if (err) throw err;
     console.log(result);
     res.json("User deleted successfuly");
   });
 });
 
-//An endpoint for updating user in the database
-adminRouter.put("/update/user", (req, res) => {
+// Endpoints for updating user in the database
+//========================================================================
+adminRouter.put("/update/username", (req, res) => {
   const query = `UPDATE user 
-    SET ${req.body.column1} = ${req.body.value1}
-    WHERE email = ${req.body.email}`;
-  db.query(query, (err, result, field) => {
+    SET username = ?
+    WHERE email = ?`;
+  const data = [req.body.value1, req.body.email];
+  db.query(query, data, (err, result, field) => {
     if (err) throw err;
     console.log(result);
     res.json("User information updated successfuly");
   });
 });
+//=============================================================================
 
 //An endpoint for creating new post
 adminRouter.post("/create/post", (req, res) => {
-  const query = `INSERT INTO post (img,title,price) VALUES (${req.body.imgUrl},${req.body.title},${req.body.price})`;
-  db.query(query, (err, result, field) => {
+  const query = `INSERT INTO post (img,title,price) VALUES (?,?,?)`;
+  const data = [req.body.imgUrl, req.body.title, req.body.price];
+  db.query(query, data, (err, result, field) => {
     if (err) throw err;
     console.log(result);
     res.json("Post added");
@@ -102,30 +114,46 @@ adminRouter.post("/create/post", (req, res) => {
 });
 
 //An endpoint for deleting a post from the database
-adminRouter.delete("delete/post", (req, res) => {
-  const query = `DELETE FROM post WHERE title=${req.body.title}`;
-  db.query(query, (err, result, field) => {
+adminRouter.delete("/delete/post", (req, res) => {
+  const query = `DELETE FROM post WHERE title=?`;
+  const data = [req.body.title];
+  db.query(query, data, (err, result, field) => {
     if (err) throw err;
     console.log(result);
     res.json("Post deleted");
   });
 });
 
-//An endpoint for updating post in the database
-adminRouter.put("/update/post", (req, res) => {
+// endpoints for updating post in the database
+//===================================================================================
+adminRouter.put("/update/post/title", (req, res) => {
   const query = `UPDATE post 
-      SET ${req.body.column1} = ${req.body.value1}
-      WHERE email = ${req.body.email}`;
-  db.query(query, (err, result, field) => {
+      SET title = ?
+      WHERE title = ?`;
+  const data = [req.body.newTitle, req.body.title];
+  db.query(query, data, (err, result, field) => {
     if (err) throw err;
     console.log(result);
     res.json("Post updated");
   });
 });
+adminRouter.put("/update/post/price", (req, res) => {
+  const query = `UPDATE post 
+      SET price = ?
+      WHERE title = ?`;
+  const data = [req.body.price, req.body.title];
+  db.query(query, data, (err, result, field) => {
+    if (err) throw err;
+    console.log(result);
+    res.json("Post updated");
+  });
+});
+//====================================================================================
 
 //An endpoint for creating new category
 adminRouter.post("/create/category", (req, res) => {
-  const query = `INSERT INTO category (name) VALUES (${req.body.name})`;
+  const query = `INSERT INTO category (name) VALUES (?)`;
+  const data = [req.body.name]
   db.query(query, (err, result, field) => {
     if (err) throw err;
     console.log(result);
@@ -134,24 +162,30 @@ adminRouter.post("/create/category", (req, res) => {
 });
 
 //An endpoint for deleting a category from the database
-adminRouter.delete("delete/category", (req, res) => {
-    const query = `DELETE FROM category WHERE name=${req.body.name}`;
-    db.query(query, (err, result, field) => {
-      if (err) throw err;
-      console.log(result);
-      res.json("Category deleted");
-    });
+adminRouter.delete("/delete/category", (req, res) => {
+  const query = `DELETE FROM category WHERE name=?`;
+  const data = [req.body.name];
+  db.query(query, (err, result, field) => {
+    if (err) throw err;
+    console.log(result);
+    res.json("Category deleted");
   });
+});
 
-//An endpoint for updating category in the database
+// Endpoints for updating category in the database
+//=================================================================================
 adminRouter.put("/update/category", (req, res) => {
-    const query = `UPDATE category 
-        SET ${req.body.column1} = ${req.body.value1}
-        WHERE name = ${req.body.name}`;
-    db.query(query, (err, result, field) => {
-      if (err) throw err;
-      console.log(result);
-      res.json("Category updated");
-    });
+  const query = `UPDATE category 
+        SET name = ?
+        WHERE name = ?`;
+  const data = [req.body.newName, req.body.name];
+  db.query(query, (err, result, field) => {
+    if (err) throw err;
+    console.log(result);
+    res.json("Category updated");
   });
-  module.exports = adminRouter;
+});
+
+//==================================================================================
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+module.exports = adminRouter;
