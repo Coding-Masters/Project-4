e079ca63a0ae3121caff6bc50aa09ac19a433b63
AbdLaswi <abed.laswii@gmail.com>
2020-11-17 02:04:53 +0200

    delete Console.logs lines

--- back-end/access/LogIn.js
+++ back-end/access/LogIn.js
@@ -12,16 +12,11 @@ const signIn = async (req, res) => {
     } else {
       if (result.length) {
         const role = `SELECT * FROM Role WHERE idRole = ?`;
-        console.log(19);
         if (await bcrypt.compare(user.password, result[0].password)) {
-          console.log(21);
           connection.query(role, result[0].Role_idRole, (err, permissions) => {
             if (err) {
               throw err;
             } else {
-              console.log(26);
-              console.log("P", payloads);
-              console.log("O", options);
               payloads = {
                 email: result[0].email,
                 permissions: permissions[0].type,
@@ -29,15 +24,11 @@ const signIn = async (req, res) => {
               options = {
                 expiresIn: process.env.TOKEN_EXPIRATION,
               };
-
               res.json(jwt.sign(payloads, process.env.SECRET, options));
               // }
             }
           });
         }
-        console.log(42);
-        console.log("P", payloads);
-        console.log("O", options);
       } else {
         res.json("Invalid login");
       }
