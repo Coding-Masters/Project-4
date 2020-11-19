49907d0a1fa7f3bada6ff8ac0e5f4e2b99efeb9d
Ismail-AbuSalah <ismail.i.abusalah@gmail.com>
2020-11-19 21:42:55 +0200

    Fix Conflict

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,6 @@ import { Route, BrowserRouter as Router, Switch ,Link } from "react-router-dom";
 import "./App.css";
 import Login from "./component/Login/login";
 import Register from "./component/registration/signup.js";
-import notFound from "./component/notFound/notFound";
 import Header from "./component/homePage/header/header";
 import Navbar from './component/profilePage/navBar';
 import Profile from "./component/profilePage/profile"
